c05a09d42cd9cafbb473e27fdaa697e125a67488
swdpcomputing <swdpcomputing@outlook.com>
2021-12-09 20:39:26 +0000

    feat(electricitytaker): 38 added split date

--- src/components/ElectricityTaker.js
+++ src/components/ElectricityTaker.js
@@ -1,11 +1,19 @@
 import "./ElectricityTaker.css";
 
 function ElectricityTaker(props) {
+  const day = props.date.toLocaleString("en-GB", { day: "2-digit" });
+  const month = props.date.toLocaleString("en-GB", { month: "long" });
+  const year = props.date.toLocaleString("en-GB", { year: "numeric" });
+
   return (
     <div className="electricity-taker">
-      <h2>{props.title}</h2>
+      <div>
+        <div>{day}</div>
+        <div>{month}</div>
+        <div>{year}</div>
+      </div>
       <div className="electricity-taker__description">
-        <h3>{props.date.toISOString()}</h3>
+        <h2>{props.title}</h2>
         <div className="electricity-taker__amount">{props.amount}kW</div>
       </div>
     </div>
